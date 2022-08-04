1550baddf7f1e67d0c50159c19ea1699b9c81e51
frantecbh <franciscomenezesbh@gmail.com>
2022-08-04 14:38:20 -0300

    Criando projeto com Typescript

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+
+app.get("/", (request, response) => {
+    return response.json({ message: "Ola Frantec" })
+})
+
+app.listen(3333, () => {
+    console.log("Server-on port 3333")
+})
+
+
